8cbff14338008a3cdef3103ae55c055f7dace2a7
sophiaBirch <birch.sophia87@gmail.com>
2023-11-17 16:02:08 -0600

    added test_television.py

--- television.py
+++ television.py
@@ -1,4 +1,7 @@
 class Television():
+    '''
+        Controls a TV remote
+    '''
     MIN_VOLUME: int = 0
     MAX_VOLUME: int = 2
     MIN_CHANNEL: int = 0
@@ -6,17 +9,29 @@ class Television():
 
 
     def __init__(self) -> None:
-        
+        '''
+            Initializes class with default values:
+                status = false
+                muted = false
+                volume = 0
+                channel = 0
+        '''
         self._status = False
         self._muted = False
         self._volume = self.MIN_VOLUME
         self._channel = self.MIN_CHANNEL
-        self._prev = self._volume
+        self._prev = self._volume ## a temp variable that stores the previous volume while the TV is muted
 
-    def power(self,):
+    def power(self):
+        '''
+            Turns power on/off
+        '''
         self._status = not(self._status)
 
     def mute(self):
+        '''
+            Mutes/unmutes and stores the previous volume
+        '''
         if self._status == True:
             self._muted = not(self._muted)
             if(self._muted == True):
@@ -26,6 +41,9 @@ class Television():
                 self._volume = self._prev
 
     def channel_up(self):
+        '''
+            Increases channel. If channel passes max channel num, cycles back to minimum channel. 
+        '''
         if self._status == True:
             if self._channel == self.MAX_CHANNEL:
                 self._channel = self.MIN_CHANNEL ## cycles channel back to min channel
@@ -33,6 +51,9 @@ class Television():
                 self._channel += 1
 
     def channel_down(self):
+        '''
+            Decreases channel. If channel passes min channel num, cycles back to max channel. 
+        '''
         if self._status == True:
             if self._channel == self.MIN_CHANNEL:
                 self._channel = self.MAX_CHANNEL ## cycles channel back to max channel
@@ -40,6 +61,9 @@ class Television():
                 self._channel -= 1
 
     def volume_up(self):
+        '''
+            Increases volume. If volume exceeds max, stays at max volume
+        '''
         if self._status == True:
             ## turns off mute
             if self._muted == True:
@@ -51,6 +75,9 @@ class Television():
                 self._volume += 1
 
     def volume_down(self):
+        '''
+            Decreases volume. If volume exceeds min, stays at min volume
+        '''
         if self._status == True:
             ## turns off mute
             if self._muted == True:
